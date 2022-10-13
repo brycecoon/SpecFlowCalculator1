6094a42d7055a2864853f00799024713f4a61937
brycecoon <brycegcoon@gmail.com>
2022-10-12 18:34:45 -0600

    finished the assignment

--- SpecFlowCalculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
+++ SpecFlowCalculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
@@ -28,6 +28,43 @@ namespace SpecFlowCalculator.Specs.StepDefinitions
             _result = _calculator.Add();
         }
 
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            _result = _calculator.Subtract();
+        }
+
+        [When(@"the two numbers are multiplied")]
+        public void WhenTheTwoNumbersAreMultiplied()
+        {
+            _result = _calculator.Multiply();
+        }
+
+        [When(@"operation \+ is done to the number (.*)")]
+        public void WhenOperation1IsDoneToTheNumber(int p0)
+        {
+            _result = _calculator.AddNum(p0);
+        }
+
+        [When(@"operation / is done to the number (.*)")]
+        public void WhenOperation2IsDoneToTheNumber(int p0)
+        {
+            _result = _calculator.DivideNum(p0);
+        }
+
+        [When(@"operation % is done to the number (.*)")]
+        public void WhenOperation3IsDoneToTheNumber(int p0)
+        {
+            _result = _calculator.ModuloNum(p0);
+        }
+
+        [When(@"operation - is done to the number (.*)")]
+        public void WhenOperation4IsDoneToTheNumber(int p0)
+        {
+            _result = _calculator.SubtractNum(p0);
+        }
+
+
         [Then("the result should be (.*)")]
         public void ThenTheResultShouldBe(int result)
         {
--- SpecFlowCalculator/Calculator.cs
+++ SpecFlowCalculator/Calculator.cs
@@ -11,5 +11,51 @@ namespace SpecFlowCalculator
         {
             return FirstNumber + SecondNumber;
         }
+
+        public int AddNum(int num)
+        {
+            FirstNumber = FirstNumber + num;
+            return FirstNumber;
+        }
+
+        public int Subtract()
+        {
+            return FirstNumber - SecondNumber;
+        }
+
+        public int SubtractNum(int num)
+        {
+            FirstNumber = FirstNumber - num;
+            return FirstNumber;
+        }
+
+        public int Multiply()
+        {
+            return FirstNumber * SecondNumber;
+        }
+
+        public int MultiplyNum(int num)
+        {
+            FirstNumber = FirstNumber * num;
+            return FirstNumber;
+        }
+
+        public int Divide()
+        {
+            return FirstNumber / SecondNumber;
+        }
+
+        public int DivideNum(int num)
+        {
+            FirstNumber = FirstNumber / num;
+            return FirstNumber;
+        }
+
+        public int ModuloNum(int num)
+        {
+            FirstNumber = FirstNumber % num;
+            return FirstNumber;
+        }
+
     }
 }
